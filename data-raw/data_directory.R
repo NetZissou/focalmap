ffe8256934689982ebf48d5d599de18b52ed72d3
Net Zhang <zhang.11091@osu.edu>
2021-09-30 09:27:19 -0400

    rename naloxone related R scripts, add opioid overdose data directory

--- data-raw/data_directory.R
+++ data-raw/data_directory.R
@@ -1,4 +1,7 @@
 ## code to prepare `naloxone_forms_data` dataset goes here
 NALOXONE_DATA_DIRECTORY <- "/fs/ess/PDE0001/project_dawn"
 
+OPIOID_OVERDOSE_DATA_DIRECTORY <- "/fs/ess/PDE0001/opioid overdose data"
+
 usethis::use_data(NALOXONE_DATA_DIRECTORY, overwrite = TRUE)
+usethis::use_data(OPIOID_OVERDOSE_DATA_DIRECTORY, overwrite = TRUE)
