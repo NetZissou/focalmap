7961a3f3debb8e9a2aa8136361ed3b1144e7cd27
Net Zhang <zhang.11091@osu.edu>
2021-09-30 11:02:19 -0400

    opioid overdose data loading function

---
+++ R/overdose_data_loading_functions.R
@@ -0,0 +1,57 @@
+#' Load focused indicators  extracted from naloxone intake form
+#'
+#' @param dir data directory on OSC that contains all of the naloxone intake forms data
+#'
+#' @return focused naloxone intake forms indicators
+#' @export
+#'
+#' @examples
+#' project_dawn_focused_indicators()
+opioid_overdose_data <- function(dir = opioidDashboard::OPIOID_OVERDOSE_DATA_DIRECTORY) {
+  data_name <- "primary_table.csv"
+  data_path <- paste0(dir, "/", data_name)
+
+  suppressWarnings(
+    suppressMessages(
+      data <-
+        readr::read_csv(
+          data_path
+        ) %>%
+        janitor::clean_names()
+    )
+  )
+
+  data <-
+    data %>%
+    dplyr::mutate(
+      # ethnicity 98% missing
+      hispanic_or_latino = !is.na(.data$ethnicity),
+      # location_type 20% missing
+      location_type = ifelse(
+        is.na(.data$location_type),
+        "Undocumented",
+        .data$location_type
+      ),
+      location_type = ifelse(
+        stringr::str_detect(.data$location_type, "Recreation"),
+        "Place of Recreation/Sport",
+        .data$location_type
+      ),
+      # destination 14% missing
+      destination = ifelse(
+        is.na(.data$destination),
+        "Undocumented",
+        .data$destination
+      )
+    ) %>%
+    dplyr::select(-.data$ethnicity) %>% # changed into hispanic_or_latino
+    # remove data with no date, latitude, longitude
+    dplyr::filter(
+      !is.na(.data$date),
+      !is.na(.data$latitude),
+      !is.na(.data$longitude)
+    )
+  return(
+    data
+  )
+}
--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,35 @@ app_dev <- function(run_check = FALSE) {
     app()
   }
 }
+
+#' @importFrom rlang ".data"
+check_na <- function(tbl, missing_only = TRUE) {
+  missing_result <-
+    tbl %>%
+    dplyr::summarise_all(
+      .funs = list(
+        ~mean(is.na(.))
+      )
+    ) %>%
+    tidyr::pivot_longer(
+      dplyr::everything(),
+      names_to = "var",
+      values_to = "missing_prop"
+    )
+  if (missing_only) {
+    missing_result <- missing_result %>%
+      dplyr::filter(.data$missing_prop > 0)
+  }
+  return(missing_result %>% dplyr::arrange(-.data$missing_prop))
+}
+
+check_na_io <- function(tbl, missing_only = TRUE) {
+  missing_info <- check_na(tbl, missing_only = missing_only)
+
+  message("Data missing info: ")
+  if (nrow(missing_info) == 0) {
+    cat("No missing values detected\n")
+  } else {
+    print(missing_info)
+  }
+}
